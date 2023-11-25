a6b096c2313b8e53dce9a2e106ce1f6cfc65b163
Harsha Vardhan Gangineni <134440804+HarshaVardhanGangineniUs@users.noreply.github.com>
2023-11-25 16:32:15 -0500

    Added quiz activity

--- app/src/main/java/com/messymaze/csquiz/MainActivity.java
+++ app/src/main/java/com/messymaze/csquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.messymaze.csquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView cardView1 = findViewById(R.id.topic_card);
+        cardView1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        });
     }
 }
